9ff699b74e414ab967ae1d6314c92d551b80eb11
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-01 23:58:59 +0100

    add except unknown scope

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,7 @@ class AuthorsController < ApplicationController
   # GET /authors
   # GET /authors.json
   def index
-    @authors = Author.all
+    @authors = Author.except_unknown
   end
 
   # GET /authors/1
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,8 @@
 class Author < ApplicationRecord
     has_many :movies
 
+    scope :except_unknown, -> { where.not(name: "UNKNOWN") }
+
     def self.unknown
         return Author.where(name: "UNKNOWN").first_or_create
     end

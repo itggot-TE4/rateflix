0ad55e7d0ac6ab2ed60c11f61459f0a79fe33cf0
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-02 00:19:37 +0100

    add select for choosing auhtor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:title)
+      params.require(:movie).permit(:title, :author_id)
     end
 end

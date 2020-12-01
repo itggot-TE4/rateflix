17592971c092b7415d0936db96f7462dbb6cb87d
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-01 23:26:05 +0100

    add NOT NULL constraint to existing data

--- db/migrate/20201201220028_add_author_ref_to_movies.rb
+++ db/migrate/20201201220028_add_author_ref_to_movies.rb
@@ -1,5 +1,18 @@
 class AddAuthorRefToMovies < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :movies, :author, null: false, foreign_key: true
+  def up
+    # add reference with NULL allowed
+    add_reference :movies, :author, foreign_key: true
+
+    # add appropriate author record to each existing movie
+    Movie.find_each do |movie|
+      movie.update!(author: Author.first)
+    end
+
+    # add NOT NULL constraint
+    change_column_null :movies, :author_id, false
+  end
+
+  def down
+    remove_reference :movies, :author, foreign_key: true
   end
 end

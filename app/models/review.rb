71c6b123f873f2030409848f7e57377cf54ab17d
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-02 02:20:35 +0100

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10 }
+  validates_uniqueness_of :user_id, :scope => :movie_id
 end

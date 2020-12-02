afafd885704723b60cb70fe45faf7c4a14bb07bf
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-02 01:32:34 +0100

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_220028) do
+ActiveRecord::Schema.define(version: 2020_12_02_002648) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2020_12_01_220028) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id", null: false
     t.index ["author_id"], name: "index_movies_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "movies", "authors"

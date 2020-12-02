6fa23c1ec375b1f5fb8e26082863483921a6e2d0
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-02 02:02:00 +0100

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_002648) do
+ActiveRecord::Schema.define(version: 2020_12_02_005640) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2020_12_02_002648) do
     t.index ["author_id"], name: "index_movies_on_author_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "movie_id", null: false
+    t.integer "rating"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +50,6 @@ ActiveRecord::Schema.define(version: 2020_12_02_002648) do
   end
 
   add_foreign_key "movies", "authors"
+  add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end

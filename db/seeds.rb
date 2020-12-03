4c477af3fe27862cdc5f2df78ae871a83f5f1892
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-03 21:30:18 +0100

    modify seeder file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+movies_with_authors = [{
+  title: 'Star Wars',
+  author: 'Jar Jar Binks',
+}, {
+  title: 'Lord of the Rings',
+  author: 'Frodo Baggins'
+}]
+Movie.create!(movies_with_authors.map {
+  | movie | {
+    title: movie[:title],
+    author: Author.create(name: movie[:author])
+  }
+})
+
+user = User.create!(:email => 'a@a.a', :password => '123123', :password_confirmation => '123123')
+
+Review.create(user: user, movie: Movie.first, rating: 5, text: "Lorem Ipsum")

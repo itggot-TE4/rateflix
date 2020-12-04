2792648011273f2e3742b38078e63fdd7c4e3299
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-04 09:29:55 +0100

    added dispatch login to button click handler

--- rateflix-vue/src/store/index.ts
+++ rateflix-vue/src/store/index.ts
@@ -1,11 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    token: ""
+  },
   mutations: {},
-  actions: {},
+  actions: {
+    async login() {
+      const response = await axios.post("http://localhost:3000/api/v1/login", {
+        user: {
+          email: "a@a.a",
+          password: "123123"
+        }
+      });
+      console.log(response.headers);
+    }
+  },
   modules: {}
 });
--- rateflix-vue/src/views/Home.vue
+++ rateflix-vue/src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="$store.dispatch('login')">Login</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 });
 </script>

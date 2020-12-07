b3faa8aab1c8143554df4c3644ff17672c4894ae
Linus Styrén <linus.styren@ga.ntig.se>
2020-12-07 23:28:10 +0100

    create Login view and add to router

---
+++ rateflix-vue/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Login",
+  methods: {
+    login() {
+      this.$store.dispatch('login');
+    }
+  }
+});
+</script>
